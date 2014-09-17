e46cfacf24dea3eb6c96f3a0a95de892110e3706
George Amvrosiadis <gamvrosi@gmail.com>
2014-09-17 15:57:10 -0400

    setup script to compile/install/setup dependencies

---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+while getopts ":dci" opt; do
+	case $opt in
+	d)
+		echo "Installing dependencies..." >&2
+
+		# Install kernel dependencies
+		sudo apt-get install build-essential kernel-package \
+			libncurses5-dev
+
+		# Install tool dependencies
+		sudo apt-get install uuid-dev libblkid-dev libz-dev liblzo2-dev \
+			libattr1-dev libacl1-dev e2fslibs-dev
+
+		echo "Done processing dependencies. Exiting." >&2
+		exit 0
+		;;
+	c)
+		# Prep the environment for future recompiles
+		export CLEAN_SOURCE=no
+		export CONCURRENCY_LEVEL=33
+		echo CLEAN_SOURCE=$CLEAN_SOURCE
+		echo CONCURRENCY_LEVEL=$CONCURRENCY_LEVEL
+
+		# (re)compile the kernel
+		cd linux-3.13.6+duet
+		time fakeroot make-kpkg --initrd --append-to-version=+duet \
+			kernel_image kernel_headers
+
+		# ...and (re)compile the btrfs tools
+		cd ../btrfs-progs-3.12+duet
+		make
+
+		# ...and (re)compile the duet tools
+		cd ../duet-progs
+		make
+
+		cd ..
+		;;
+	i)
+		# Install the kernel
+		sudo dpkg -i linux-headers-3.13.6+duet_3.13.6+duet-10.00.Custom_amd64.deb
+		sudo dpkg -i linux-image-3.13.6+duet_3.13.6+duet-10.00.Custom_amd64.deb
+
+		# Install the btrfs tools (in /usr/local/bin)
+		cd btrfs-progs-3.12+duet
+		sudo make install
+
+		# Install the duet tools (in /usr/local/bin)
+		cd ../duet-progs
+		sudo make install
+
+		cd ..
+		;;
+	\?)
+		echo "Invalid option: -$OPTARG" >&2
+		;;
+	esac
+done
